369c8807d5e661072a448bc613cb18edf3b6fae6
Colin McFadden <cmcfadden@gmail.com>
2015-11-18 15:52:48 -0600

    test for minification of 3d viewer

--- assets/js/buildMinified.sh
+++ assets/js/buildMinified.sh
@@ -1,7 +1,7 @@
 cat serializeForm.js dateWidget.js template.js > master.js
-yui-compressor -o master.min.js master.js
+yuicompressor -o master.min.js master.js
 cat assetView.js drawers.js > assetMaster.js
-yui-compressor -o assetMaster.min.js assetMaster.js
+yuicompressor -o assetMaster.min.js assetMaster.js
 cat search.js searchForm.js > searchMaster.js
-yui-compressor -o searchMaster.min.js searchMaster.js 
+yuicompressor -o searchMaster.min.js searchMaster.js 
 
