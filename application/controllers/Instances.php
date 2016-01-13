c01d5217e2ca7054c123af40e11c5de6e50c21d1
Colin McFadden <cmcfadden@gmail.com>
2016-01-13 16:27:01 -0600

    undo mule changes, change bucket creaiton policy

--- application/controllers/Instances.php
+++ application/controllers/Instances.php
@@ -185,7 +185,7 @@ class Instances extends Instance_Controller {
 		}
 
 		if($s3Secret && $s3Key && $s3InstanceName) {
-			$bucketName = "elevator." . $s3InstanceName;
+			$bucketName = "elevator-" . $s3InstanceName;
 			$s3Client = null;
 			try {
 				$s3Client = new Aws\S3\S3Client(['region'=>'us-east-1', 'version'=>'2006-03-01', 'credentials'=>['secret'=>$s3Secret, 'key'=>$s3Key]]);
@@ -318,7 +318,7 @@ class Instances extends Instance_Controller {
 			}
 
 
-			$newUser = "elevator.bucket_user" . $s3InstanceName;
+			$newUser = "elevator-bucket_user" . $s3InstanceName;
 			$client = new Aws\Iam\IamClient(['region'=>'us-east-1', 'version'=>'2010-05-08', 'credentials'=>['secret'=>$s3Secret, 'key'=>$s3Key]]);
 
 			try {
