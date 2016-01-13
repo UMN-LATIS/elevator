522cf226e5f3dee0f7a0eecb3dff0ca57f277304
Colin McFadden <cmcfadden@gmail.com>
2016-01-13 16:25:34 -0600

    more changes to mule

--- assets/js/mule2.js
+++ assets/js/mule2.js
@@ -1092,7 +1092,7 @@
             self.request_date = new Date();
 
             self.headers = self.settings.headers;
-            self.headers['host'] = self.settings.auth.bucket + ".s3.amazonaws.com";
+            self.headers['host'] = "s3.amazonaws.com";
 
             var date_string = [
                 self.settings.auth.date.getUTCFullYear(),
@@ -1121,7 +1121,7 @@
 
             delete self.headers['host'];  // keep this header only for hashing
 
-            var url = location.protocol + "//" + self.settings.auth.bucket + ".s3.amazonaws.com/" + self.settings.key;
+            var url = location.protocol + "//" + ".s3.amazonaws.com/" + self.settings.auth.bucket + "/" + self.settings.key;
             var first = true;
             for(var key in self.settings.querystring) {
                 if(self.settings.querystring.hasOwnProperty(key)) {
