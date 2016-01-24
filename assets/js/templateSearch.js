84d3b0790e351d78e39a17c00be349f945ab61f9
Colin McFadden <cmcfadden@gmail.com>
2016-01-24 09:29:47 -0600

    fix multicollectoon advanced search

--- assets/js/templateSearch.js
+++ assets/js/templateSearch.js
@@ -95,6 +95,6 @@ $(document).on("click", ".addAnotherSpecific", function() {
 $(document).on("click", ".addAnotherCollection", function() {
     var collectionCopy = $(this).closest('.form-group');
     var newCollection = $(collectionCopy).clone(false);
+    newCollection.find("input[type='text']").val("");
     $(collectionCopy).after(newCollection);
-
 });
