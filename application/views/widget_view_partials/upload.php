92c9c34a1035e2a393ac1341f5239b48224d0fcb
Colin McFadden <cmcfadden@gmail.com>
2016-01-28 16:14:11 -0600

    preload amazon urls

--- application/views/widget_view_partials/upload.php
+++ application/views/widget_view_partials/upload.php
@@ -1,9 +1,31 @@
 <p><strong><?=$widgetModel->getLabel()?>:</strong></p>
-<ul class="fileList">
+<ul class="">
 	<?foreach($widgetModel->fieldContentsArray as $fieldContent):?>
+
+	<?
+			try {
+				$fileHandler = $fieldContent->getFileHandler();
+				if($fileHandler) {
+
+					$retina = $fileHandler->getPreviewTiny(true)->getURLForFile();
+					$standard = $fileHandler->getPreviewTiny(false)->getURLForFile();
+				}
+			}
+			catch (Exception $e) {
+				if($fileHandler && $fileHandler->icon) {
+					$retina = "/assets/icons/48px/" . $fileHandler->icon;
+					$standard = "/assets/icons/48px/" . $fileHandler->icon;
+				}
+				else {
+					$retina = "/assets/icons/48px/_blank.png";
+					$standard = "/assets/icons/48px/_blank.png";
+				}
+
+			}
+	?>
 	<li>
 		<div class="thumbnailFixedSize">
-			<a href="#<?=$fieldContent->fileId?>"><img data-fileobjectid="<?=$fieldContent->fileId?>" class="lazy loadView" data-retina="<?=instance_url("fileManager/tinyImageByFileId/" . $fieldContent->fileId . "/true/true") ?>" data-src="<?=instance_url("fileManager/tinyImageByFileId/" . $fieldContent->fileId . "/false/true") ?>"></a>
+			<a href="#<?=$fieldContent->fileId?>"><img data-fileobjectid="<?=$fieldContent->fileId?>" class="lazy loadView" data-retina="<?=$retina?>" data-src="<?=$standard?>"></a>
 		</div>
 	</li>
 	<?endforeach?>
