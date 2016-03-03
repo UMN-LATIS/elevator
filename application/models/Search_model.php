024db50158ff1b0cc38a0e985f210c90a44cd9ff
Colin McFadden <cmcfadden@gmail.com>
2016-03-03 16:59:04 -0600

    curses, fooled by negation again

--- application/models/Search_model.php
+++ application/models/Search_model.php
@@ -20,7 +20,7 @@ class search_model extends CI_Model {
 		$params['index'] = 'elevator';
     	$params['type']  = 'asset';
     	$params['id']    = $asset->getObjectId();
-    	if($params['id'] || strlen($params['id']<5)) {
+    	if(!$params['id'] || strlen($params['id']<5)) {
     		// if you don't pass an id, elasticsearch will eat all your data
     		return;
     	}
