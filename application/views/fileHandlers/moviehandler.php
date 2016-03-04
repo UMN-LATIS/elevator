ac7b307ca7b8921c91801ac6661c2bb569897c8d
Colin McFadden <cmcfadden@gmail.com>
2016-03-04 09:30:15 -0600

    Fix stupid typo

--- application/views/fileHandlers/moviehandler.php
+++ application/views/fileHandlers/moviehandler.php
@@ -11,21 +11,21 @@ $mediaArray = array();
 
 if(isset($fileContainers['streaming'])) {
   $entry["type"] = "hls";
-  $entry["file"] = stripHTTP($fileContainers['streaming'])->getProtectedURLForFile());
+  $entry["file"] = stripHTTP($fileContainers['streaming']->getProtectedURLForFile());
   $entry["label"] = "Streaming";
   $mediaArray["streaming"] = $entry;
 }
 
 if(isset($fileContainers['mp4sd'])) {
   $entry["type"] = "mp4";
-  $entry["file"] = stripHTTP($fileContainers['streaming'])->getProtectedURLForFile());
+  $entry["file"] = stripHTTP($fileContainers['streaming']->getProtectedURLForFile());
   $entry["label"] = "SD";
   $mediaArray["mp4sd"] = $entry;
 }
 
 if(isset($fileContainers['mp4hd'])) {
   $entry["type"] = "mp4";
-  $entry["file"] = stripHTTP($fileContainers['streaming'])->getProtectedURLForFile());
+  $entry["file"] = stripHTTP($fileContainers['streaming']->getProtectedURLForFile());
   $entry["label"] = "HD";
   $mediaArray["mp4hd"] = $entry;
 }
