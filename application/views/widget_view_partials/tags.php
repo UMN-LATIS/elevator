9f1bb9ed41b9826f7c9511be31c0613ad52109d5
Colin McFadden <cmcfadden@gmail.com>
2016-03-30 14:02:20 -0500

    fix linked tags

--- application/views/widget_view_partials/tags.php
+++ application/views/widget_view_partials/tags.php
@@ -2,7 +2,7 @@
 <ul class="tagList">
 	<?foreach($widgetModel->fieldContentsArray as $fieldContent):?>
 	<li><?foreach($fieldContent->tags as $tag):?>
-		<A href="<?=instance_url("/search/querySearch/". rawurlencode($tag))?>"><?=$tag?></a>
+		<?=$widgetModel->getClickToSearch()?"<A href=" .instance_url("/search/querySearch/". rawurlencode($tag)) .">".$tag."</a>":$tag?>
 	<?endforeach?></li>
 	<?endforeach?>
 </ul>
