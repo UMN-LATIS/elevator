a33af6ca82bf8f02257de7152beda51715f1aaf0
Colin McFadden <cmcfadden@gmail.com>
2016-03-30 14:02:20 -0500

    fix linked tags

--- application/views/widget_view_partials/multiselect.php
+++ application/views/widget_view_partials/multiselect.php
@@ -1,7 +1,8 @@
 <strong><?=$widgetModel->getLabel()?>:</strong>
 <ul>
 	<?foreach($widgetModel->fieldContentsArray as $fieldContent):?>
-	<li><?=join(" : ", array_filter($fieldContent->fieldContents))?></li>
+<li><?=$content=join(" : ", array_filter($fieldContent->fieldContents)); $widgetModel->getClickToSearch()?"<A href=" .instance_url("/search/querySearch/". rawurlencode($content)) .">".$content."</a>":$content?>
+	</li>
 	<?endforeach?>
 </ul>
 
