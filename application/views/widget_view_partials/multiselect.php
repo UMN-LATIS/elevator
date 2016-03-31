b2248a567316d017870e7153d5e3c545f801b088
Colin McFadden <cmcfadden@gmail.com>
2016-03-31 13:07:30 -0500

    missing change to multiselect

--- application/views/widget_view_partials/multiselect.php
+++ application/views/widget_view_partials/multiselect.php
@@ -1,7 +1,7 @@
 <strong><?=$widgetModel->getLabel()?>:</strong>
 <ul>
 	<?foreach($widgetModel->fieldContentsArray as $fieldContent):?>
-<li><?=$content=join(" : ", array_filter($fieldContent->fieldContents)); $widgetModel->getClickToSearch()?"<A href=" .instance_url("/search/querySearch/". rawurlencode($content)) .">".$content."</a>":$content?>
+<li><?$content=join(" : ", array_filter($fieldContent->fieldContents)); echo $widgetModel->getClickToSearch()?"<A href=" .instance_url("/search/querySearch/". rawurlencode($content)) .">".$content."</a>":$content?>
 	</li>
 	<?endforeach?>
 </ul>
