7f8aded48dcb600b12863aa164054c9c2c28df0f
Colin McFadden <cmcfadden@gmail.com>
2016-04-20 11:52:28 -0500

    make sure extra spaces don't confuse things

--- application/views/widget_view_partials/select.php
+++ application/views/widget_view_partials/select.php
@@ -2,7 +2,7 @@
 <ul>
 	<?foreach($widgetModel->fieldContentsArray as $fieldContent):?>
 	<?if(array_keys($widgetModel->parsedFieldData["selectGroup"]) !== range(0, count($widgetModel->parsedFieldData["selectGroup"]) - 1)):?>
-		<li><?=$widgetModel->getClickToSearch()?"<A href=\"".instance_url("/search/querySearch/". rawurlencode($widgetModel->parsedFieldData["selectGroup"][$fieldContent->fieldContents])) ."\">".$widgetModel->parsedFieldData["selectGroup"][$fieldContent->fieldContents]."</a>":$widgetModel->parsedFieldData["selectGroup"][$fieldContent->fieldContents];?></li>
+		<li><?=$widgetModel->getClickToSearch()?"<A href=\"".instance_url("/search/querySearch/". rawurlencode($widgetModel->parsedFieldData["selectGroup"][$fieldContent->fieldContents])) ."\">".$widgetModel->parsedFieldData["selectGroup"][trim($fieldContent->fieldContents)]."</a>":$widgetModel->parsedFieldData["selectGroup"][trim($fieldContent->fieldContents)];?></li>
 	<?else:?>
 		<li><?=$widgetModel->getClickToSearch()?"<A href=\"".instance_url("/search/querySearch/". rawurlencode($fieldContent->fieldContents)) ."\">".$fieldContent->fieldContents."</a>":$fieldContent->fieldContents;?></li>
 	<?endif?>
