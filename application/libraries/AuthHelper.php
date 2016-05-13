cbe12f55e65f4c0c5e4939c2083dfdb0284c6a7a
Colin McFadden <cmcfadden@gmail.com>
2016-05-13 16:46:13 -0500

    refactor autocomplete

--- application/libraries/AuthHelper.php
+++ application/libraries/AuthHelper.php
@@ -35,4 +35,36 @@ class AuthHelper
 		return array();
 	}
 
+	public function autocompleteUsername($partialUsername) {
+		$CI =& get_instance();
+		$result = $CI->doctrine->em->getRepository("Entity\User")->createQueryBuilder('u')
+				->where('u.instance= :instance')
+				->andWhere('u.displayName LIKE :name')
+				->setParameter('instance', $CI->instance)
+				->setParameter('name', '%'.$partialUsername.'%')
+				->getQuery()
+				->getResult();
+
+		//TODO: limit number of users
+		//TODO: only search local uesrs
+		$userMatches = $CI->doctrine->em->getRepository("Entity\User")->findBy(["username"=>$partialUsername]);
+
+		foreach($userMatches as $user) {
+			$tempArray = ["name"=>$user->getDisplayName(), "email"=>$user->getEmail(), "completionId"=>$user->getId(), "username"=>$user->getUsername()];
+			$outputArray[$user->getId()] = $tempArray;
+		}
+		$i=0;
+		foreach($result as $user) {
+			$tempArray = ["name"=>$user->getDisplayName(), "email"=>$user->getEmail(), "completionId"=>$user->getId(), "username"=>$user->getUsername()];
+			if(!array_key_exists($user->getId(), $outputArray)) {
+				$outputArray[$user->getId()] = $tempArray;
+			}
+			$i++;
+			if($i>10) {
+				break;
+			}
+		}
+		return $outputArray;
+	}
+
 }
