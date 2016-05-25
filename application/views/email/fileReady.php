17ebcd2ace3f52e18d9f55f72f3f9f5a3f3484ec
Colin McFadden <cmcfadden@gmail.com>
2016-05-25 18:13:45 -0500

    fix typo in file ready email

--- application/views/email/fileReady.php
+++ application/views/email/fileReady.php
@@ -12,7 +12,7 @@
 	<body>
 		<div class="row">
 			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				The file you requested is ready.  You cand download it from <A href="<?=$pathToFile?>"<?=$pathToFile?></a>.
+				The file you requested is ready.  You cand download it from <A href="<?=$pathToFile?>"><?=$pathToFile?></a>.
 			</div>
 		</div>
 
