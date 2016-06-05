05f60ca9adf19cd11e1ad9c7e5159bd5a1b78c5a
Ubuntu <ubuntu@ip-10-11-0-203.ec2.internal>
2016-06-05 14:20:10 +0000

    fix overlap

--- assets/leaflet/Leaflet.elevator.js
+++ assets/leaflet/Leaflet.elevator.js
@@ -30,13 +30,22 @@ L.TileLayer.Elevator = L.TileLayer.extend({
         return tile;
     },
 
-    // getTileSize: function() {
-    // 	var size = L.TileLayer.prototype.getTileSize();
-    // 	console.log(size);
-    // 	return size;
+    getTileSize: function() {
+		var map = this._map,
+		    tileSize = L.GridLayer.prototype.getTileSize.call(this),
+		    zoom = this._tileZoom + this.options.zoomOffset,
+		    zoomN = this.options.maxNativeZoom;
 
+	    tileSize.x = tileSize.x + this.options.overlap; // with deepzoom, our tile size removes the overlap, but leaflet needs it.
+	    tileSize.y = tileSize.y + this.options.overlap;
+		// increase tile size when overscaling
+		var outputSize= zoomN !== null && zoom > zoomN ?
+				tileSize.divideBy(map.getZoomScale(zoomN, zoom)).round() :
+				tileSize;
+		
+		return outputSize;
+    },
 
-    // },
 _computeImageAndGridSize: function () { // thanks https://github.com/turban/Leaflet.Zoomify
 	var options = this.options,
 	tileSize = options.tileSize || 256
