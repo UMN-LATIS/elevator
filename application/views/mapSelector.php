8ad740941ee535ef554dac97552c5617a127b87a
Ubuntu <ubuntu@ip-10-11-0-203.ec2.internal>
2016-06-07 16:37:19 +0000

    store address in addition to gps

--- application/views/mapSelector.php
+++ application/views/mapSelector.php
@@ -3,7 +3,7 @@
 	<div class="form-group">
 		<label for="input" class="col-sm-2 control-label">Address:</label>
 		<div class="col-sm-4">
-			<input type="text" class="form-control address" value="" title="">
+			<input type="text" class="form-control address" <?=isset($addressName)?"name='$addressName'":null?> <?=isset($addressContents)?"value='$addressContents'":null?>  title="">
 		</div>
 		<div class="col-sm-4">
 			<button type="button" class="btn btn-primary searchMap">Search</button>
