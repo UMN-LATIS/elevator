51f8eb6a87397866a2cac1fc1bf307550ac9b998
Colin McFadden <cmcfadden@gmail.com>
2016-06-08 11:33:49 -0500

    add second plus button

--- application/views/widget_form_partials/widget_footer.php
+++ application/views/widget_form_partials/widget_footer.php
@@ -1,2 +1,5 @@
+	<?if($widgetModel->getAllowMultiple()):?>
+				<button type="button" class="pull-right btn btn-default addAnother"><span class="glyphicon glyphicon-plus"></span></button>
+			<?endif?>
 	</div> <!-- close control-group -->
 </div> <!-- close tab-pane -->
