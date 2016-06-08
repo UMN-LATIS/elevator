1c8f3297604decb4febf8984ba1b888e2c045ed3
Colin McFadden <cmcfadden@gmail.com>
2016-06-08 11:42:30 -0500

    refactor adding a bit so we're not group constrained

--- application/views/widget_form_partials/widget_footer.php
+++ application/views/widget_form_partials/widget_footer.php
@@ -1,2 +1,6 @@
+
 	</div> <!-- close control-group -->
+		<?if($widgetModel->getAllowMultiple()):?>
+		<button type="button" class="pull-right btn btn-default addAnother bottomButton"><span class="glyphicon glyphicon-plus"></span></button>
+	<?endif?>
 </div> <!-- close tab-pane -->
