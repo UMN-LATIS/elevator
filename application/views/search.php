a3a2c51ff87fbd0ecd6d642e7083638d4abb4181
Colin McFadden <cmcfadden@gmail.com>
2016-07-07 13:34:56 -0500

    fix typo

--- application/views/search.php
+++ application/views/search.php
@@ -48,7 +48,7 @@
 				 <div class="clearfix"></div>
 				<div class="paginationBlock">
 					<ul class="pager">
-					  <li><a href="#" class="nextPage">Load Moret</a></li>
+					  <li><a href="#" class="nextPage">Load More</a></li>
 					</ul>
 				</div>
 			</div>
