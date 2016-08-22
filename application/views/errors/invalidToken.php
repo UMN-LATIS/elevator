8179bc65bfaf5bae91a6000a89be0b350858f2ec
Ubuntu <ubuntu@ip-10-11-0-203.ec2.internal>
2016-08-22 14:10:45 +0000

    add error handling

---
+++ application/views/errors/invalidToken.php
@@ -0,0 +1,9 @@
+
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Invalid Token</h3>
+	</div>
+	<div class="panel-body">
+		An invalid message was received from Google.  Try restarting your web brower and trying again.
+	</div>
+</div>
