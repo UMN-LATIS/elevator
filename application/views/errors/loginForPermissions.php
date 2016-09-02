a5a6bfeec079a25f4decfafece9fe82b65d20669
Colin McFadden <cmcfadden@gmail.com>
2016-09-02 12:23:37 -0500

    make messages more friendly for unauthed users

--- application/views/errors/loginForPermissions.php
+++ application/views/errors/loginForPermissions.php
@@ -1,1 +1,3 @@
-You may have access to additional derivatives if you log in using the link in the upper right.
+You may have access to additional derivatives if you sign in
+<?if($embedded):?>.
+<?else:?> using the link in the upper right.<?endif?>
