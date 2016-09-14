dd0a6785db7b7b56485ea0c3ff2701cfab77a553
Colin McFadden <cmcfadden@gmail.com>
2016-09-14 15:10:09 -0500

    add helpful comment

--- application/views/fileHandlers/moviehandler.php
+++ application/views/fileHandlers/moviehandler.php
@@ -99,6 +99,7 @@ if(typeof objectId == 'undefined') {
       </p>
 
     <?elseif(isset($fileObject->sourceFile->metadata["spherical"])):?>
+    <?# this file must be uploaded to s3 for these to work in safari as of 2016 ?>
             <iframe frameborder=0 width="100%" height=480px scrolling="no" allowfullscreen src="http://s3.amazonaws.com/elevator-assets/vrview/index.html?video=<?=urlencode(striphttp($fileContainers['mp4hd1080']->getProtectedURLForFile()))?>&is_stereo=<?=isset($fileObject->sourceFile->metadata["stereo"])?"true":"false"?>"></iframe>
     <?else:?>
     <div id="videoElement">Loading the player...</div>
