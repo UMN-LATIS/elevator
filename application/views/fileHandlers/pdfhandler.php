f7fa803acd8545c2fb0704cbf12f433a622bf51d
Ubuntu <ubuntu@ip-10-11-0-203.ec2.internal>
2016-11-03 16:32:47 +0000

    add automatic OCR of PDFs in cases where source PDF doesnt have OCR text.  Need to test the worst case giant-pdf-without-text case.

--- application/views/fileHandlers/pdfhandler.php
+++ application/views/fileHandlers/pdfhandler.php
@@ -10,6 +10,9 @@ $targetFile = null;
 if(isset($fileContainers['shrunk_pdf'])) {
 	$targetFile = $fileContainers['shrunk_pdf']->getProtectedURLForFile();
 }
+else if(isset($fileContainers['ocr_pdf'])) {
+	$targetFile = $fileContainers['ocr_pdf']->getProtectedURLForFile();
+}
 else if(isset($fileContainers['pdf'])) {
 	$targetFile = $fileContainers['pdf']->getProtectedURLForFile();
 }
@@ -58,7 +61,7 @@ $menuArray['download'] = $downloadArray;
 	<div class="col-md-12">
 <?endif?>
 <?if($targetFile):?>
-	<iframe class="vrview" frameborder=0 width="100%" height=480px scrolling="no" allowfullscreen src="/assets/pdf/web/viewer.html?file=<?=urlencode(striphttp($targetFile))?>#zoom=page-fit&"></iframe>
+	<iframe class="vrview" frameborder=0 width="100%" height=480px scrolling="no" allowfullscreen src="/assets/pdf/web/viewer.html?file=<?=urlencode(striphttp($targetFile))?>#zoom=page-fit&page=0"></iframe>
 <?else:?>
 	<img src="<?=isset($fileContainers['thumbnail2x'])?stripHTTP($fileContainers['thumbnail2x']->getProtectedURLForFile()):"/assets/icons/512px/pdf.png"?>" class="img-responsive embedImage" style="width: 50%; margin-left:auto; margin-right:auto"/>
 <?endif?>
