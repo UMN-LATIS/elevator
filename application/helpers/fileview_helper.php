28d69f9268312e2a620b9f60ae615b2b5f5b679d
Ubuntu <ubuntu@ip-10-11-0-203.ec2.internal>
2016-11-23 17:35:02 +0000

    general cleanup

--- application/helpers/fileview_helper.php
+++ application/helpers/fileview_helper.php
@@ -11,9 +11,13 @@ function renderFileMenu($menuArray) {
 			if($entryKey == "Location") {
 				$outputText .= '<li class="list-group-item assetDetails"><strong>Location: </strong><A href="#mapModal"  data-toggle="modal" data-latitude="' . $fileInfoEntry['latitude'] . '" data-longitude="' . $fileInfoEntry['longitude'] . '">View Location</a></li>';
 			}
+			else if($entryKey == "Exif") {
+				$outputText .= '<li class="list-group-item assetDetails"><strong>' . $entryKey . ':</strong> <a href="" class="exifToggle" data-fileobject="' . $fileInfoEntry . '">View Exif</a></li>';
+			}
 			else {
 				$outputText .= '<li class="list-group-item assetDetails"><strong>' . $entryKey . ':</strong> ' . htmlentities($fileInfoEntry, ENT_QUOTES) . ' </li>';	
 			}
+
 			
 		}
 		$outputText .= '</ul>\'></span>';
@@ -36,6 +40,11 @@ function renderFileMenu($menuArray) {
 
 	if($menuArray['excerpt']) {
 		$outputText .= ' <span data-toggle="collapse" data-target="#excerptGroup" class="glyphicon glyphicon-time excerptTooltip" data-toggle="tooltip" title="Create Excerpt"></span>';
+	}
+
+	if($menuArray['zoom']) {
+		$outputText .= '<span></span><input type="range" class="zoom-range">
+			<span class="canFullscreen glyphicon glyphicon-resize-full" data-toggle="tooltip" title="Fullscreen"></span>';
 	}
 	
 	$outputText .= "</div></div>";
--- application/views/fileHandlers/objhandler.php
+++ application/views/fileHandlers/objhandler.php
@@ -20,6 +20,37 @@ $embedLink = str_replace("https:", "", $embedLink);
 $embed = htmlentities('<iframe width="560" height="480" src="' . $embedLink . '" frameborder="0" allowfullscreen></iframe>', ENT_QUOTES);
 
 
+$fileInfo = [];
+$fileInfo["File Type"] = "3D Object";
+$fileInfo["Original Name"] = $fileObject->sourceFile->originalFilename;
+$fileInfo["File Size"] = $fileObject->sourceFile->metadata["filesize"];
+
+
+
+$menuArray['fileInfo'] = $fileInfo;
+
+
+$downloadArray = [];
+
+if(isset($fileContainers['nxs']) && $fileContainers['nxs']->ready) {
+  $downloadArray["Download Derivative (nxs)"] = instance_url("fileManager/getDerivativeById/". $fileObjectId . "/nxs");
+}
+
+if(isset($fileContainers['ply']) && $fileContainers['ply']->ready) {
+  $downloadArray["Download Derivative (ply)"] = instance_url("fileManager/getDerivativeById/". $fileObjectId . "/ply");
+}
+
+if(isset($fileContainers['stl']) && $fileContainers['stl']->ready) {
+  $downloadArray["Download Derivative (stl)"] = instance_url("fileManager/getDerivativeById/". $fileObjectId . "/stl");
+}
+
+if($allowOriginal) {
+  $downloadArray['Download Original'] = instance_url("fileManager/getOriginal/". $fileObjectId);
+}
+
+$menuArray['download'] = $downloadArray;
+
+
 ?>
 <link type="text/css" rel="stylesheet" href="/assets/3dviewer/stylesheet/3dhop.css"/>
 <?if(defined('ENVIRONMENT') && ENVIRONMENT == "development") :?>
@@ -234,36 +265,7 @@ var loadedCallback = function() {
 <?if(!$embedded):?>
 
 
-<div class="row infoRow ">
-  <div class="col-md-12">
-    <span class="glyphicon glyphicon-info-sign infoPopover" data-placement="bottom" data-toggle="popover" title="File Info" data-html=true data-content='<ul class="list-group">
-      <li class="list-group-item"><strong>File Type: </strong> OBJ</li>
-      <li class="list-group-item assetDetails"><strong>Original Name: </strong><?=$fileObject->sourceFile->originalFilename?></li>
-      <li class="list-group-item assetDetails"><strong>File Size: </strong><?=byte_format($fileObject->sourceFile->metadata["filesize"])?></li>
-    </ul>'></span>
-      <span class="glyphicon glyphicon-download infoPopover" data-placement="bottom" data-toggle="popover" title="Download" data-html="true" data-content='
-          <ul>
-          <?if(isset($fileContainers['nxs'])):?>
-            <li class="list-group-item assetDetails"><a href="<?=instance_url("fileManager/getDerivativeById/". $fileObjectId . "/nxs")?>">Download Derivative (nxs)</a></li>
-          <?endif?>
-          <?if(isset($fileContainers['ply'])):?>
-            <li class="list-group-item assetDetails"><a href="<?=instance_url("fileManager/getDerivativeById/". $fileObjectId . "/ply")?>">Download Derivative (ply)</a></li>
-          <?endif?>
-          <?if(isset($fileContainers['stl'])):?>
-            <li class="list-group-item assetDetails"><a href="<?=instance_url("fileManager/getDerivativeById/". $fileObjectId . "/stl")?>">Download Derivative (stl)</a></li>
-          <?endif?>
-      <?if($allowOriginal):?>
-      <li class="list-group-item assetDetails"><a href="<?=instance_url("fileManager/getOriginal/". $fileObjectId)?>">Download Original</a></li>
-      <?endif?>
-      </ul>'></span>
-    <span class="glyphicon glyphicon-share infoPopover" data-placement="bottom" data-toggle="popover" title="Share" data-html="true" data-content='<ul class="list-group">
-        <?if($allowOriginal):?>
-            <li class="list-group-item assetDetails"><strong>Embed: </strong><input class="form-control embedControl" value="<?=htmlspecialchars($embed, ENT_QUOTES)?>"></li>
-          <?endif?>
-    </ul>'></span>
-  </div>
-</div>
-
+<?=renderFileMenu($menuArray)?>
 <script>
 $(function ()
 {
