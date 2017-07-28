f660e4cd120988674178bd156b3c0019a1ab57b3
Colin McFadden <cmcfadden@gmail.com>
2017-07-28 14:38:45 -0500

    woops

--- application/models/Search_model.php
+++ application/models/Search_model.php
@@ -486,7 +486,7 @@ class search_model extends CI_Model {
 				$i++;
 
 			}
-			$searchParams['body']['query']['bool']['minimum_should_match'] = 1;
+			// $searchParams['body']['query']['bool']['minimum_should_match'] = 1;
 
 		}
 
