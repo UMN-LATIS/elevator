d1f4b043d35f378993957ccbdf18f87924e87704
Ubuntu <ubuntu@ip-10-11-0-86.ec2.internal>
2018-02-06 22:48:06 +0000

    fix zero indxing issue

--- assets/js/search.js
+++ assets/js/search.js
@@ -533,27 +533,27 @@ function prepTimeline() {
 					formattedStart = Date.utc.create(t);
 
 					newItem.start_date.year = formattedStart.getFullYear();
-					newItem.start_date.month = formattedStart.getMonth();
-					newItem.start_date.day = formattedStart.getDay();
+					newItem.start_date.month = formattedStart.getMonth() + 1;
+					newItem.start_date.day = formattedStart.getDay() + 1;
 
 				}
 				
 				if(date.end["numeric"] && date.end["numeric"].length>0) {
 					newItem.end_date = {};
 					endTime = parseInt(date.end["numeric"], 10);
-					
+
 					if(geoTime) {
 						endYear = -1 * Math.round(Math.abs((endTime+ (1970*31556900)) / 31556900));
 						newItem.end_date.year = endYear;
 					}
 					else {
 						t = new Date(1970,0,1);
-						t.setSeconds(startTime);
+						t.setSeconds(endTime);
 						formattedEnd = Date.utc.create(t);
 						newItem.end_date.year = formattedEnd.getFullYear();
-						newItem.end_date.month = formattedEnd.getMonth();
-						newItem.end_date.day = formattedEnd.getDay();
-
+						newItem.end_date.month = formattedEnd.getMonth() + 1;
+						newItem.end_date.day = formattedEnd.getDay() + 1;
+						console.log(newItem);
 					}
 
 				}
@@ -564,7 +564,7 @@ function prepTimeline() {
 				newElement = $("<a/>", {"href": basePath + "asset/viewAsset/" + match.objectId, "text": match.title});
 				newItem.text.headline = newElement.prop("outerHTML");
 				newItem.text.text = html;
-				
+				console.log(newItem);
 				if(match.hasOwnProperty("primaryHandlerThumbnail2x")) {
 					newItem.media = {};
 					newItem.media.thumb = match.primaryHandlerThumbnail2x;
