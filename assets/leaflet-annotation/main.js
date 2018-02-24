654298e63fdfa5fd123339897a71fd2bb81bbe8c
Colin McFadden <cmcfadden@gmail.com>
2018-02-24 13:02:27 -0600

    try a different size

--- assets/leaflet-annotation/main.js
+++ assets/leaflet-annotation/main.js
@@ -178,7 +178,7 @@ var arrowOptions = {
     popupContent: function(data) {
         return '<em>' + data.title + '</em>'
     },
-    arrowheadLength: 0.2,
+    arrowheadLength: 0.4,
     color: 'blue'
 }
 
