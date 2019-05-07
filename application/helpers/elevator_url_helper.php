1d8768872ea80aaee4a0048adb2a6196b056c3d4
Colin McFadden <cmcfadden@gmail.com>
2019-05-07 16:16:58 -0500

    redo url helper to use CI native but patched

--- application/helpers/elevator_url_helper.php
+++ application/helpers/elevator_url_helper.php
@@ -28,46 +28,54 @@ function getClickToSearchLink($widgetModel, $linkText, $displayText = null) {
 	if(!$displayText) {
 		$displayText = $linkText;
 	}
-
+	
 	$linkText = str_ireplace("?", "", $linkText);
 	$linkText = str_ireplace("...", "", $linkText);
-
+	
 	if($widgetModel->getClickToSearchType() == 0) {
 		return "<A href=\"".instance_url("/search/querySearch/". rawurlencode($linkText)) ."\">".$displayText."</a>";
 	}
-
+	
 	if($widgetModel->getClickToSearchType() == 1) {
 		return "<A href=\"".instance_url("/search/scopedQuerySearch/". $widgetModel->getFieldTitle() . "/". rawurlencode($linkText)) ."\">".$displayText."</a>";
 	}
-
-
+	
+	
 }
-
-function autolink_elevator($value) {
-	$protocols = array('http', 'mail');
-	$attributes = ["target"=>"_blank"];
-	// Link attributes
-	$attr = '';
-	foreach ($attributes as $key => $val) {
-		$attr = ' ' . $key . '="' . htmlentities($val) . '"';
-	}
-	
-	$links = array();
-	
-	// Extract existing links and tags
-	$value = preg_replace_callback('~(<a .*?>.*?</a>|<.*?>)~i', function ($match) use (&$links) { return '<' . array_push($links, $match[1]) . '>'; }, $value);
-	
-	// Extract text links for each protocol
-	foreach ((array)$protocols as $protocol) {
-		switch ($protocol) {
-			case 'http':
-			case 'https':   $value = preg_replace_callback('~(?:(https?)://([^\s<]+)|(www\.[^\s<]+?\.[^\s<]+))(?<![\.,:])~i', function ($match) use ($protocol, &$links, $attr) { if ($match[1]) $protocol = $match[1]; $link = $match[2] ?: $match[3]; return '<' . array_push($links, "<a $attr href=\"$protocol://$link\">$link</a>") . '>'; }, $value); break;
-				case 'mail':    $value = preg_replace_callback('~([^\s<]+?@[^\s<]+?\.[^\s<]+)(?<![\.,:])~', function ($match) use (&$links, $attr) { return '<' . array_push($links, "<a $attr href=\"mailto:{$match[1]}\">{$match[1]}</a>") . '>'; }, $value); break;
-				case 'twitter': $value = preg_replace_callback('~(?<!\w)[@#](\w++)~', function ($match) use (&$links, $attr) { return '<' . array_push($links, "<a $attr href=\"https://twitter.com/" . ($match[0][0] == '@' ? '' : 'search/%23') . $match[1]  . "\">{$match[0]}</a>") . '>'; }, $value); break;
-				default:        $value = preg_replace_callback('~' . preg_quote($protocol, '~') . '://([^\s<]+?)(?<![\.,:])~i', function ($match) use ($protocol, &$links, $attr) { return '<' . array_push($links, "<a $attr href=\"$protocol://{$match[1]}\">{$match[1]}</a>") . '>'; }, $value); break;
+function autolink_elevator($str, $type = 'both', $popup = FALSE)
+{
+	// Find and replace any URLs.
+	if ($type !== 'email' && preg_match_all('#(\w*://|www\.)[^\s()<>;]+\w#i', $str, $matches, PREG_OFFSET_CAPTURE | PREG_SET_ORDER))
+		{
+			// Set our target HTML if using popup links.
+			$target = ($popup) ? ' target="_blank"' : '';
+			
+			// We process the links in reverse order (last -> first) so that
+			// the returned string offsets from preg_match_all() are not
+			// moved as we add more HTML.
+			foreach (array_reverse($matches) as $match)
+			{
+				// $match[0] is the matched string/link
+				// $match[1] is either a protocol prefix or 'www.'
+				//
+				// With PREG_OFFSET_CAPTURE, both of the above is an array,
+				// where the actual value is held in [0] and its offset at the [1] index.
+				$a = '<a href="'.(strpos($match[1][0], '/') ? '' : 'http://').$match[0][0].'"'.$target.'>'.$match[0][0].'</a>';
+				$str = substr_replace($str, $a, $match[0][1], strlen($match[0][0]));
+			}
 		}
-	}
 		
-		// Insert all link
-		return preg_replace_callback('/<(\d+)>/', function ($match) use (&$links) { return $links[$match[1] - 1]; }, $value);
-}
+		// Find and replace any emails.
+		if ($type !== 'url' && preg_match_all('#([\w\.\-\+]+@[a-z0-9\-]+\.[a-z0-9\-\.]+[^[:punct:]\s])#i', $str, $matches, PREG_OFFSET_CAPTURE))
+		{
+			foreach (array_reverse($matches[0]) as $match)
+			{
+				if (filter_var($match[0], FILTER_VALIDATE_EMAIL) !== FALSE)
+				{
+					$str = substr_replace($str, mailto($match[0]), $match[1], strlen($match[0]));
+				}
+			}
+		}
+		
+		return $str;
+	}
--- application/views/widget_view_partials/text.php
+++ application/views/widget_view_partials/text.php
@@ -7,7 +7,7 @@
 		$content = $fieldContent->fieldContents;
 	}
 	else {
-		$content = autolink_elevator($fieldContent->fieldContents);
+		$content = autolink_elevator($fieldContent->fieldContents, 'both', TRUE);
 	}?>
 	<li><?=$widgetModel->getClickToSearch()?getClickToSearchLink($widgetModel, $fieldContent->fieldContents):$content;?></li>
 	<?endforeach?>
