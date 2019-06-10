1edb42965d8030e6bfefca382fc2c17e8cff8fc1
Colin McFadden <cmcfadden@gmail.com>
2019-06-10 08:47:39 -0500

    change my mind on that

--- application/views/widget_view_partials/date.php
+++ application/views/widget_view_partials/date.php
@@ -8,18 +8,17 @@
 	if($fieldContent->label) {
 		$labelString = "<span class='date_label'>" . $fieldContent->label."</span>";
 	}
-	$dateString = "<span class='date_value'>" . $fieldContent->start["text"];
+	$dateString = $fieldContent->start["text"];
 	if($fieldContent->range) {
 		$dateString .= " - " . $fieldContent->end["text"];
 	}
 
-	$dateString .= "</span>";
 
 	if($labelString) {
-		$outputString .= $labelString . " (" . $dateString . ")";
+		$outputString .= $labelString . " <span class='date_value'>(" . $dateString . ")</span>";
 	}
 	else {
-		$outputString = $dateString;
+		$outputString ="<span class='date_value'>" . $dateString . "</span>";
 	}
 	
 	?>
