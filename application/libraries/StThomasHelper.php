f3611c2d64f48b2d37664103dbf49fcafb791701
Colin McFadden <cmcfadden@gmail.com>
2020-02-04 13:50:45 -0600

    make stthomas search users across instances

--- application/libraries/StThomasHelper.php
+++ application/libraries/StThomasHelper.php
@@ -134,6 +134,37 @@ class StThomasHelper extends AuthHelper
 		// return $this->CI->load->view("authHelpers/autoRedirect", null, true);
 	}
 
+	public function autocompleteUsername($partialUsername) {
+		$CI =& get_instance();
+		$result = $CI->doctrine->em->getRepository("Entity\User")->createQueryBuilder('u')
+				->where('u.displayName LIKE :name')
+				->setParameter('name', '%'.$partialUsername.'%')
+				->getQuery()
+				->getResult();
+
+		//TODO: limit number of users
+		//TODO: only search local uesrs
+		$userMatches = $CI->doctrine->em->getRepository("Entity\User")->findBy(["username"=>$partialUsername]);
+		$outputArray = array();
+		foreach($userMatches as $user) {
+			$tempArray = ["name"=>$user->getDisplayName(), "email"=>$user->getEmail(), "completionId"=>$user->getId(), "username"=>$user->getUsername()];
+			$outputArray[$user->getId()] = $tempArray;
+		}
+		$i=0;
+		foreach($result as $user) {
+			$tempArray = ["name"=>$user->getDisplayName(), "email"=>$user->getEmail(), "completionId"=>$user->getId(), "username"=>$user->getUsername()];
+			if(!array_key_exists($user->getId(), $outputArray)) {
+				$outputArray[$user->getId()] = $tempArray;
+			}
+			$i++;
+			if($i>10) {
+				break;
+			}
+		}
+		return $outputArray;
+	}
+
+
 	public function remoteLogout() {
 		
 		if ($this->shibboleth->hasSession() && $this->CI->config->item("shibbolethLogout")) {
