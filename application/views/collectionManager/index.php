797bb8bf6d74ca881023769dc25744f27ea5c58c
Colin McFadden <cmcfadden@gmail.com>
2020-04-22 08:27:55 -0500

    show parent in collection list

--- application/views/collectionManager/index.php
+++ application/views/collectionManager/index.php
@@ -1,8 +1,9 @@
 <div class="row rowContainer">
-	<div class="col-md-9">
+	<div class="col-md-12">
 		<table class="table table-striped" id="collectionList">
 <thead>
 						<th>Collection</th>
+						<th>Parent</th>
 						<th>Edit Permissions</th>
 						<th>Edit Collection</th>
 						<th>Share</th>
@@ -13,6 +14,7 @@
 					<?php foreach ($collections as $collection) : ?>
 					<tr>
 						<td><?= $collection->getTitle(); ?></td>
+						<td><?= $collection->getParent()?$collection->getParent()->getTitle():null?></td>
 						<td><a href="<?= instance_url("permissions/edit/collection/{$collection->getId()}"); ?>">Edit Permissions</a></td>
 						<td><a href="<?= instance_url("collectionManager/edit/{$collection->getId()}"); ?>">Edit</a></td>
 						<td><a href="<?= instance_url("collectionManager/share/{$collection->getId()}"); ?>">Share</a></td>
@@ -33,6 +35,9 @@
         "order": [[ 0, "asc" ]],
         "columns":[
             {
+                "sortable": true
+            },
+			{
                 "sortable": true
             },
             {
