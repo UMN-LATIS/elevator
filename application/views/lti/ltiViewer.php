aa749fadf2b9b34de6bf4e4bb8d2409e6fa5b1bf
Colin McFadden <cmcfadden@gmail.com>
2021-01-07 16:04:06 -0600

    fix lti redirect

--- application/views/lti/ltiViewer.php
+++ application/views/lti/ltiViewer.php
@@ -9,6 +9,6 @@ sessionStorage.includeMetadata = null;
 sessionStorage.returnURL = "<?=$returnURL?>";
 
 document.cookie = '_check_is_passive=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-window.location = "<?=instance_url()?>";
+window.location = "<?=instance_url("/")?>";
 
 </script>
