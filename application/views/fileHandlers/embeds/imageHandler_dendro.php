b6b988e8753ca78553c9c988d957d141b8461fc4
Colin McFadden <cmcfadden@gmail.com>
2021-04-16 15:11:07 -0500

    stop propagation for magnify

--- application/views/fileHandlers/embeds/imageHandler_dendro.php
+++ application/views/fileHandlers/embeds/imageHandler_dendro.php
@@ -229,6 +229,9 @@ void main(void){
 		});
 
 		L.DomEvent.on(window, 'keydown', function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			
 			if(e.keyCode == 76 && e.getModifierState("Control")) {
 				if (imageMap.hasLayer(magnifyingGlass)) {
 					imageMap.removeLayer(magnifyingGlass);
