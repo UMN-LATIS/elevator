29b14217b640e5bc8b9ef009b91ed8171a0bd588
Colin McFadden <cmcfadden@gmail.com>
2021-08-31 18:46:59 +0100

    fix collection display to show full hierarchies

--- application/views/asset/sidebar.php
+++ application/views/asset/sidebar.php
@@ -12,14 +12,18 @@
 </div>
 <?if($sidebarAssetModel->assetTemplate->getShowCollection()):?>
 <?$collection = $this->collection_model->getCollection($sidebarAssetModel->getGlobalValue("collectionId"));?>
-<div class="row">
+<div class="row collectionWidget">
 			<div class="col-md-12 assetWidget">
 				<ul>
+				
 					<strong>Collection:</strong>
-						<li><a href="<?=instance_url("collections/browseCollection/". $collection->getId())?>"><?=$collection->getTitle()?></a>
-						</li>
+					<ul class="collectionList">
+							<? foreach(array_reverse($this->collection_model->getFullHierarchy($collection->getId())) as $collection):?>
+							<li><a href="<?=instance_url("collections/browseCollection/". $collection->getId())?>"><?=$collection->getTitle()?></a>
+							</li>
+							<?endforeach?>
+					</ul>
 				</ul>
 			</div>
 		</div>
-</div>
 <?endif?>
