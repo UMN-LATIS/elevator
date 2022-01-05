a2d233f3675f1ba09b4d3391be24ad2fb6432e7c
Colin McFadden <cmcfadden@gmail.com>
2022-01-05 09:20:02 -0600

    add templates

--- application/views/fileHandlers/embeds/imageHandler_dendro.php
+++ application/views/fileHandlers/embeds/imageHandler_dendro.php
@@ -49,7 +49,7 @@ if($widgetObject->parentWidget->dendroFields) {
 	<script src="/assets/leaflet-treering/node_modules/leaflet-easybutton/src/easy-button.js"></script>
 	<script src="/assets/leaflet-treering/node_modules/leaflet-dialog/Leaflet.Dialog.js"></script>
     <script src="/assets/leaflet-treering/Leaflet.TileLayer.GL.js"></script>
-
+	
 <script src="/assets/leaflet/Leaflet.elevator.js"></script>
 <script src="/assets/leaflet-treering/leaflet.magnifyingglass.js"></script>
 <script type="application/javascript" src="/assets/leaflet-treering/leaflet-treering.js"></script>
@@ -306,8 +306,13 @@ void main(void){
 		canSave = true;
 		<?endif?>
 		popoutURL = "<?=stripHTTP(instance_url("asset/getEmbed/" . $fileObject->getObjectId() . "/null/true"));?>";
-		treering = new LTreering(imageMap, "/assets/leaflet-treering/",{ppm:baseLayer.options.pixelsPerMillimeter, saveURL: saveURL, savePermission:canSave, popoutUrl: popoutURL, 'initialData': sideCar, 'assetName': "<?=$fileObject->parentObject->getAssetTitle(true)?>", 'datingInner': innerYear, 'hasLatewood': <?=$haveLateWood?"true":"false"?>});
-    	treering.loadInterface();
+
+		$.get("/assets/leaflet-treering/templates.html", function (data) {
+			$("body").append(data);
+			treering = new LTreering(imageMap, "/assets/leaflet-treering/",{ppm:baseLayer.options.pixelsPerMillimeter, saveURL: saveURL, savePermission:canSave, popoutUrl: popoutURL, 'initialData': sideCar, 'assetName': "<?=$fileObject->parentObject->getAssetTitle(true)?>", 'datingInner': innerYear, 'hasLatewood': <?=$haveLateWood?"true":"false"?>}, baseLayer, layer );
+    		treering.loadInterface();
+		});
+
     	// if(saveURL != "") {
     	// 	treering.addSaveButton();
     	// }
