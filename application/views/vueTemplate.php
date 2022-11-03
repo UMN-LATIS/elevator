dcb5982328e7b856a4d706efa8319f41b45c8c68
Elevator Dev (ubuntu) <latistecharch@umn.edu>
2022-11-03 21:26:10 +0000

    remove google font links

--- application/views/vueTemplate.php
+++ application/views/vueTemplate.php
@@ -5,11 +5,6 @@
   <meta charset="UTF-8" />
   <link rel="icon" href="/favicon.ico" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-
   <title>
     <?php
     $this->template->title->default(isset($this->instance)
@@ -29,7 +24,7 @@
           },
         },
         arcgis: {
-          apiKey: "<?=$this->config->item('arcgis_access_token')?>",
+          apiKey: "<?= $this->config->item('arcgis_access_token') ?>",
         },
       },
     }
