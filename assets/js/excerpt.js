6891b35250a69be92fcf5ddb48b9216488cd2ca3
Elevator Dev (ubuntu) <latistecharch@umn.edu>
2023-07-28 14:35:12 +0000

    add new ui and json support for viewExcerpt

--- assets/js/excerpt.js
+++ assets/js/excerpt.js
@@ -36,7 +36,36 @@ function setPlayBounds(startTime, endTime) {
 }
 
 function getTime() {
-	var startTime = jwplayer("videoElement").getPosition();
-	return startTime;
+	return jwplayer("videoElement").getPosition();
 }
 
+// listen for message from parent window to set start and end times
+window.addEventListener('message',  function handleSetPlayBoundsMessage(event) {
+	if (event.data.type !== 'setPlayBounds') {
+		return;
+	}
+
+	if (Number.isNaN(event.data.startTime) || Number.isNaN(event.data.endTime)) {
+		console.log(event.data.startTime, event.data.endTime);
+		throw new Error('start and end times must be set to set play bounds');
+	}
+
+	console.log('setting play bounds', event.data.startTime, event.data.endTime);
+
+	setPlayBounds(event.data.startTime, event.data.endTime);
+	
+	// once we've set the play bounds, 
+	// we don't need to listen for this message anymore
+	// window.removeEventListener('message', handleSetPlayBoundsMessage);
+});
+
+window.addEventListener('message',  function handleGetTimeMessage(event) {
+	if (event.data.type !== 'getTime') {
+		return;
+	}
+
+	window.parent.postMessage({
+		type: 'getTimeResponse',
+		currentScrubberPosition: getTime(),
+	}, '*');
+});
