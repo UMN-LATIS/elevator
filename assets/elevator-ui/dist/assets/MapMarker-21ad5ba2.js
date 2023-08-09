f3b50c08e23430565cf56b89c862837cb7969ecb
Elevator Dev (ubuntu) <latistecharch@umn.edu>
2023-08-09 15:08:41 +0000

    rev ui

---
+++ assets/elevator-ui/dist/assets/MapMarker-21ad5ba2.js
@@ -0,0 +1,2 @@
+import{_ as f}from"./main-bf072d50.js";export{f as default};
+//# sourceMappingURL=MapMarker-21ad5ba2.js.map
--- assets/elevator-ui/dist/assets/MapMarker-e4c28002.js
+++
@@ -1,2 +0,0 @@
-import{_ as f}from"./main-d4b592c9.js";export{f as default};
-//# sourceMappingURL=MapMarker-e4c28002.js.map
