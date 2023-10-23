5ffee4fa598a49cc8c0a0fd81d87f90b01ed466f
Elevator Dev (ubuntu) <latistecharch@umn.edu>
2023-10-23 22:56:49 +0000

    add lti 1.3 support

--- application/controllers/api/v1/Lti13.php
+++ application/controllers/api/v1/Lti13.php
@@ -1,32 +1,148 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+use Packback\Lti1p3\LtiOidcLogin;
+use Packback\Lti1p3\LtiMessageLaunch;
+use Packback\Lti1p3\ImsStorage\ImsCache;
+use Packback\Lti1p3\ImsStorage\ImsCookie;
+use Packback\Lti1p3\LtiException;
+use Packback\Lti1p3\LtiDeepLinkResource;
+use Packback\Lti1p3\LtiDeepLinkResourceIframe;
+
 class lti13 extends Instance_Controller {
 
 	public function __construct()
 	{
+    Firebase\JWT\JWT::$leeway = 20;
 		parent::__construct();
+    $this->load->helper('url');
 
 	}
 
 
-	public function launchLTI()
-	{
-        $returnURL = $this->input->post('content_item_return_url');
-        echo $this->load->view("lti/ltiViewer", ["instance"=>$this->instance, "returnURL"=>$returnURL], true);
-	}
+  public function login() {
+    $this->load->library("LTI13Database");
+    echo "Heh";
+    return LtiOidcLogin::new(new LTI13Database, new ImsCache, new ImsCookie)
+        ->doOidcLoginRedirect(instance_url("api/v1/lti13/launch"))
+        ->doRedirect();
+}
 
 
-	public function ltiConfig() 
-	{
+  public function launch() {
+    if(isset($_REQUEST['error']) && $_REQUEST['error'] == 'launch_no_longer_valid') {
+      $exception = new \Exception($_REQUEST['error_description']);
+      echo "fail";
+      // if (app()->bound('sentry')) {
+      //     app('sentry')->captureException($exception);
+      // }
+      // return view("errors.500", ["exception"=>$exception]);
+  }
 
-        if($this->input->get("Instance_Name")) {
-            $this->config->set_item("instance_name", $this->input->get("Instance_Name"));
-            Instance_Controller::setInstance();
-            $this->config->set_item("instance_absolute", Instance_Controller::getAbsolutePath());
-        }
+   try {
+      $launch = LtiMessageLaunch::new(
+          new LTI13Database, 
+          new ImsCache, 
+          new ImsCookie, 
+          new \Packback\Lti1p3\LtiServiceConnector(
+              new ImsCache, 
+              new \GuzzleHttp\Client([
+                  'timeout' => 30,
+              ])
+          )
+      )
+      ->validate();
+  }
+  catch (LtiException $e) {
 
-        echo $this->load->view("lti/ltiConfig", ["instance"=>$this->instance], true);
-	}
+      // canvas needs to update for new window to work https://github.com/instructure/canvas-lms/commit/811a1194cabccc1b3fb22aa3d13d64cde547116d#diff-79b6cd1bab1e82354966238b3d72cfa8fffb6357a61d2454bf4aba1c85b96a5e
+            echo '<script>
+            window.parent.postMessage(
+  {
+    messageType: "requestFullWindowLaunch",
+    subject: "requestFullWindowLaunch",
+    data: {
+      url: "' . instance_url("api/v1/lti13/launch") . '",
+      launchType: "new_window",
+      "placement": "editor_button",
+    }
+  },
+  "*"
+)           
+      </script>';
+//       echo '<script>
+//       window.parent.postMessage(
+// {
+// messageType: "requestFullWindowLaunch",
+// data: "' . instance_url("api/v1/lti13/launch") . '"
+// },
+// "*"
+// )           
+//       </script>
+//       <h1>Canvas Launch Error</h1>
+//       <p>' . $e->getMessage() . "</p>";
+      return;
+  }
+  $launchData = $launch->getLaunchData();
+  if($launchData["https://purl.imsglobal.org/spec/lti/claim/message_type"] != "LtiDeepLinkingRequest")  {
+    echo "fail";
+    return;
+  }
+  $deepLinkSettings = $launchData["https://purl.imsglobal.org/spec/lti-dl/claim/deep_linking_settings"];
+  $returnURL = $deepLinkSettings["deep_link_return_url"];;
+  
+      echo $this->load->view("lti/ltiViewer", ["instance"=>$this->instance, "returnURL"=>$returnURL, "ltiVersion"=>"1.3", "launchId"=>$launch->getLaunchId()], true);
+
+
+  }
+
+  public function config() {
+    $this->load->helper('url');
+    // key generated with https://mkjwk.org
+    $configArray = [
+        "title" => "Elevator",
+        "description" => "Elevator Digital Asset Management System",
+        "oidc_initiation_url" => instance_url("api/v1/lti13/login"),
+        "target_link_uri" => instance_url("api/v1/lti13/launch"),
+        "extensions" => [
+            [
+                "domain" => $_SERVER['HTTP_HOST'],
+                "tool_id" => "elevator",
+                "platform" => "canvas.instructure.com",
+                "settings" => [
+                    "privacy_level" => "public",
+                    "text" => "Launch Elevator",
+                    "icon_url" => site_url("/assets/images/elevatorIconTiny.png"),
+                    "placements" => [
+                            [
+                                "text" => "Elevator",
+                                "enabled" => true,
+                                "placement" => "editor_button",
+                                "message_type" => "LtiDeepLinkingRequest",
+                                "target_link_uri" => instance_url("lti13/launch"),
+                                "canvas_icon_class" => "icon-lti"
+                            ]
+                        ]
+                    ]
+                ]
+        ],
+        "public_jwk" => [
+            "kty"=> "RSA",
+            "e"=> "AQAB",
+            "use"=> "sig",
+            "kid"=> "sig-1698080546",
+            "alg"=> "RS256",
+            "n"=> "vwUtR5esRCNqoKf4np1m0kQpyp5-zwfnUImPsn8-wq-RgOMo4ffj-cX0z2tvnlZ_KXWKZ9ER-1V-Ez9Ukg2mw_RRjQXk1qc5DUBzLNhzOoiUUZn_AJ-_Bs0unpGAKfnWNNpZs0-so056blAIjMZgCU_zlTN6zbSp9QXoHnPgXw6pL2pfTF5tZgpK4MCZTBKQdc4PbpTxFZmltN8jUTPkMyL6uTluFydv13IFMYTTx58mHdtPkg8ZaFKMnsXnHFKpsn8afX2pvegur1iQ2lYUYqqWNWF2KpjBwPnVEqCbZutoJn4fQID962AtOtOM__ZKmxat47aSrME6x0J2-m8Zww"
+        ],
+        "custom_fields" => [  
+            "canvas_integration_id" =>'$Canvas.user.sisSourceId',
+            "user_username" => '$User.username',
+            "canvas_user_id" => '$Canvas.user.id',
+            "canvas_course_id" => '$Canvas.course.id'
+        ]
+    ];
+
+    return render_json($configArray);
+}
 
     public function ltiPayload() {
         if(!$this->user_model->userLoaded) {
@@ -59,25 +175,45 @@ class lti13 extends Instance_Controller {
         else {
           $embedLink = instance_url("/asset/getEmbed/" . $objectId.  "/null/true?" . http_build_query($targetQuery));
         }
-        
-        $string = ('{
-        "@context": "http://purl.imsglobal.org/ctx/lti/v1/ContentItem",
-        "@graph": [
-          {
-            "@type": "ContentItem",
-            "mediaType": "text/html",
-            "placementAdvice": {
-              "presentationDocumentTarget": "iframe",
-              "displayWidth": "640",
-              "displayHeight": "480"
-            },
-            "text": "",
-            "title": "Embedded Asset",
-            "url": "' . $embedLink  .'"
-          }
-        ]
-        }');
-        echo $string;
+
+
+        $launchId = $this->input->post("launchId");
+        if(!$launchId) {
+          echo "HeH";
+          return;
+        }
+
+        $launch = LtiMessageLaunch::fromCache($launchId, new LTI13Database, new ImsCache,  new \Packback\Lti1p3\LtiServiceConnector(
+          new ImsCache, 
+          new \GuzzleHttp\Client([
+              'timeout' => 30,
+          ])
+      ));
+        $deepLink = $launch->getDeepLink();
+        $deepLinkResource = new LtiDeepLinkResource();
+        $deepLinkResource->setType("link");
+        $deepLinkResource->setUrl($embedLink);
+        $deepLinkResource->setTitle("test");
+        $deepLinkResource->setIframe(new LtiDeepLinkResourceIframe(640,480, $embedLink));
+        // $string = ('{
+        // "@context": "http://purl.imsglobal.org/ctx/lti/v1/ContentItem",
+        // "@graph": [
+        //   {
+        //     "@type": "ContentItem",
+        //     "mediaType": "text/html",
+        //     "placementAdvice": {
+        //       "presentationDocumentTarget": "iframe",
+        //       "displayWidth": "640",
+        //       "displayHeight": "480"
+        //     },
+        //     "text": "",
+        //     "title": "Embedded Asset",
+        //     "url": "' . $embedLink  .'"
+        //   }
+        // ]
+        // }');
+// var_dump($deepLinkResource->toArray());
+        echo $deepLink->outputResponseForm([$deepLinkResource]);
         return;
     }
 }
--- application/views/lti/ltiViewer.php
+++ application/views/lti/ltiViewer.php
@@ -7,7 +7,8 @@ sessionStorage.timeStamp = null;
 sessionStorage.entangledSecret = null;
 sessionStorage.includeMetadata = null;
 sessionStorage.returnURL = "<?=$returnURL?>";
-
+sessionStorage.ltiVersion = "<?=$ltiVersion?>";
+sessionStorage.launchId = "<?=$launchId??null?>";
 document.cookie = '_check_is_passive=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 window.location = "<?=instance_url("/")?>";
 
