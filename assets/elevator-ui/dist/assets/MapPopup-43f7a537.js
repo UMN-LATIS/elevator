996f1df84ce89c9e4a715b30cfaedd7b00311edf
Elevator Dev (ubuntu) <latistecharch@umn.edu>
2024-12-19 17:29:43 +0000

    rev ui

---
+++ assets/elevator-ui/dist/assets/MapPopup-43f7a537.js
@@ -0,0 +1,2 @@
+import{d as r,r as t,i as p,M as a,o as s,a as u,b as c,c as l,e as _}from"./main-5bb47230.js";const m=r({__name:"MapPopup",setup(i){const o=t(null),e=p(a);return s(()=>{if(!e)throw new Error("Cannot add popup. Marker context is null");e.createPopup(o)}),u(()=>{if(!e)throw new Error("Cannot remove popup. Marker context is null");e.removePopup()}),(n,f)=>(c(),l("div",{ref_key:"popupContainerRef",ref:o,class:"map-popup"},[_(n.$slots,"default")],512))}});export{m as default};
+//# sourceMappingURL=MapPopup-43f7a537.js.map
