142c9dc3c2455630e247b87d344bc1230c610787
James Johnson <johnsojr@umn.edu>
2025-01-28 11:10:33 -0600

    add a single entry point for 3dviewer

---
+++ assets/3dviewer/js/index.mjs
@@ -0,0 +1,9 @@
+// Import all the modules for a single entry point
+import "./spidergl";
+import "./presenter";
+import "./ply";
+import "./trackball_pantilt";
+import "./trackball_sphere";
+import "./trackball_turntable";
+import "./trackball_turntable_pan";
+import "./init";
