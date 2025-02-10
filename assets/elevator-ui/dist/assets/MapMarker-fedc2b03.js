719dce79c48194b6eed731c0cd8f5e535e5e9518
Colin McFadden <colin@hedge.video>
2025-02-10 11:25:28 +0700

    remove committed dist

--- assets/elevator-ui/dist/assets/MapMarker-fedc2b03.js
+++
@@ -1,2 +0,0 @@
-import{_ as f}from"./main-5bb47230.js";export{f as default};
-//# sourceMappingURL=MapMarker-fedc2b03.js.map
--- documentation/buildScript.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-BASEPATH=$1
-pandoc -s -S --toc -V documentclass=report --template=/Users/colin/Documents/Development/divergentDocumentation/pandocTemplates/customTemplate.tex --variable fontsize=12pt $BASEPATH.md --latex-engine=xelatex -o "$BASEPATH Manual.pdf"
