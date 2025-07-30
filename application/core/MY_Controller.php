61f9f91cd6d82626de39d78a84896955f88ba01d
James Johnson <johnsojr@umn.edu>
2025-07-30 12:14:00 -0500

    prevent s3 endpoints from closing session early

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -58,7 +58,16 @@ class MY_Controller extends CI_Controller {
 		$userId = $this->session->userdata('userId');
 		// HACK HACK HACK
 		// Close the session if we're not going to be doing a login, prevent session locks in case of hung urls
-		if($this->uri->segment(2) && strtolower($this->uri->segment(2)) !== "loginmanager" && strtolower($this->uri->segment(1)) !== "loginmanager" && strtolower($this->uri->segment(1)) !== "shibboleth"  && strtolower($this->uri->segment(1)) !== "shibboleth.sso") {
+		$segment1 = strtolower($this->uri->segment(1));
+		$segment2 = strtolower($this->uri->segment(2));
+		$shouldCloseSession = $this->uri->segment(2) && 
+			$segment2 !== "loginmanager" && 
+			$segment1 !== "loginmanager" && 
+			$segment1 !== "shibboleth" && 
+			$segment1 !== "shibboleth.sso" && 
+			$segment1 !== "s3";
+		
+		if($shouldCloseSession) {
 			session_write_close();
 		}
 		if($userId) {
