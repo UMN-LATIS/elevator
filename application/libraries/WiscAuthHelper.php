8d79b7ccfa786079d064292adc929c853355eada
Colin McFadden <colin@hedge.video>
2025-11-11 17:00:14 -0600

    add wisc auth helper

--- application/libraries/WiscAuthHelper.php
+++ application/libraries/WiscAuthHelper.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* OU Helper
+* Wisc Helper
 */
 
 
@@ -15,7 +15,7 @@
 require_once("AuthHelper.php");
 class WiscAuthHelper extends AuthHelper
 {
-	// public $authTypes = [UNIT_TYPE=>["name"=>UNIT_TYPE, "label"=>UNIT_TYPE], JOB_TYPE=>["name"=>JOB_TYPE, "label"=>"Job Code"], COURSE_TYPE=>["name"=>COURSE_TYPE, "label"=>COURSE_TYPE], DEPT_COURSE_TYPE=>["name"=>DEPT_COURSE_TYPE, "label"=>DEPT_COURSE_TYPE, "helpText"=>"Use % for wildcard, like DEPT.NUMBER% to include all sections."], STATUS_TYPE=>["name"=>STATUS_TYPE, "label"=>"Student Status"], EMPLOYEE_TYPE=>["name"=>EMPLOYEE_TYPE, "label"=>"Employee Type"]];
+	public $authTypes = [GROUP_MEMBERSHIP=>["name"=>GROUP_MEMBERSHIP, "label"=>GROUP_MEMBERSHIP]];
 
 	public function __construct()
 	{
@@ -87,7 +87,7 @@ class WiscAuthHelper extends AuthHelper
 
 	public function populateUserData($user) {
 		$userData = array();
-		$coursesTaught = array();
+		$groupMembership = array();
 		$courses = array();
 		$deptCourses = array();
 		$jobCodes = array();
@@ -98,58 +98,12 @@ class WiscAuthHelper extends AuthHelper
 		$CI =& get_instance();
 		$map = $CI->session->userdata("userAttributesCache");
 		if (isset($map) && is_array($map)) {
-			// if($this->shibboleth->getAttributeValue('eduCourseMember')) {
-			// 	$courseArray = explode(";",$this->shibboleth->getAttributeValue('eduCourseMember'));
-
-			// 	// hacky stuff to deal with the way this info is passed in
-			// 	// todo: learn about the actual standard for eduCourseMember
-			// 	foreach($courseArray as $course) {
-			// 		$explodedString = explode("@", $course);
-			// 		if(count($explodedString)>0) {
-			// 			$role = $explodedString[0];
-			// 		}
-			// 		$courseString = explode("/", $course);
-			// 		$courseId = $courseString[8];
-			// 		$deptCourse = $courseString[6];
-			// 		if($role == "Instructor") {
-						
-			// 			$courseName = $courseString[6];
-			// 			$coursesTaught[$courseId + 0] = $courseName;
-			// 			$deptCoursesTaught[$courseName] = $courseName;
-			// 		}
-			// 		$courses[] = $courseId + 0;
-			// 		$deptCourses[] = $deptCourse;
-			// 	}
-			// }
-			
-			// if($this->shibboleth->getAttributeValue('umnJobSummary')) {
-			// 	$jobCodeSummary = explode(";",$this->shibboleth->getAttributeValue('umnJobSummary'));
-			// 	foreach($jobCodeSummary as $jobCode) {
-			// 		$jobCodeArray = explode(":", $jobCode);
-			// 		if(isset($jobCodeArray[2])) {
-			// 			$jobCodes[] = $jobCodeArray[2] + 0;
-			// 		}
-			// 		if(isset($jobCodeArray[10])) {
-			// 			$units[] = $jobCodeArray[10];
-			// 		}
-
-			// 	}
-			// }
-			
-			// if($this->shibboleth->getAttributeValue('umnRegSummary')) {
-			// 	$regSummary = explode(";",$this->shibboleth->getAttributeValue('umnRegSummary'));
-			// 	foreach($regSummary as $studentCode) {
-			// 		$studentStatusArray = explode(":", $studentCode);
-			// 		if(isset($studentStatusArray[12]) && strlen($studentStatusArray[12]) == 4) {
-			// 			$studentStatus[] = $studentStatusArray[12];
-			// 		}
-			// 	}	
-			// }
-
-			// if($this->shibboleth->getAttributeValue('eduPersonAffiliation')) {
-			// 	$employeeType = explode(";",$this->shibboleth->getAttributeValue('eduPersonAffiliation'));
-			// }
-			
+			if(isset($map['eduPersonAffiliation'])) {
+				$groupMembership = is_array($map['eduPersonAffiliation']) ? $map['eduPersonAffiliation'] : [$map['eduPersonAffiliation']];
+			}				
+		}
+		else {
+			return false;
 		}
 
 
@@ -161,18 +115,29 @@ class WiscAuthHelper extends AuthHelper
 		// foreach($units as $unit) {
 		// 	$unitHints[$unit] = $unit;
 		// }
+
+		$groupHints = array();
+		foreach($groupMembership as $group) {
+			$groupHints[$group] = $group;
+		}
 		// $userData[UNIT_TYPE] = ["values"=>$units, "hints"=>$unitHints];
 		// $userData[STATUS_TYPE] = ["values"=>array_unique($studentStatus), "hints"=>["UGRD"=>"Undergraduate", "GRAD"=>"Graduate"]];
 
-		// $userData[EMPLOYEE_TYPE] = ["values"=>array_unique($employeeType), "hints"=>["Faculty" => "Faculty","Student" => "Student","Staff" => "Staff","Alum " => "Alum" ,"Member" => "Member","Affiliate" => "Affiliate"]];
-
+		$userData[GROUP_MEMBERSHIP] = ["values"=>array_unique($groupMembership), "hints"=>$groupHints];
+		
+		$CI =& get_instance();
+		$CI->logging->logError($user->getEmail(), $map);
+		$CI->logging->logError($user->getEmail(), $userData);
 		return $userData;
 
 	}
 
+
 	public function getGroupMapping($userData) {
 		$outputArray = array();
-
+		if(!$userData || !is_array($userData)) {
+			return $outputArray;
+		}
 		foreach($userData as $key=>$value) {
 			$outputArray[$key] = $value["values"];
 		}
