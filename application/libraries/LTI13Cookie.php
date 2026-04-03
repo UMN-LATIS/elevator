189067fd7d9800ffe9d2d667acb1768c95c0b60b
Colin McFadden <colin@E28.local>
2026-04-03 10:18:18 -0500

    rev cookie

--- application/libraries/LTI13Cookie.php
+++ application/libraries/LTI13Cookie.php
@@ -4,6 +4,15 @@ use Packback\Lti1p3\Interfaces\ICookie;
 
 class LTI13Cookie implements ICookie
 {
+    private function isSecureRequest(): bool
+    {
+        if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
+            return true;
+        }
+
+        return (($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '') === 'https');
+    }
+
     public function getCookie(string $name): ?string
     {
         return $_COOKIE[$name] ?? null;
@@ -11,12 +20,14 @@ class LTI13Cookie implements ICookie
 
     public function setCookie(string $name, string $value, int $exp = 3600, array $options = []): void
     {
+        $isSecure = $this->isSecureRequest();
+
         $defaultOptions = [
             'expires' => time() + $exp,
             'path' => '/',
-            'secure' => (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'),
+            'secure' => $isSecure,
             'httponly' => true,
-            'samesite' => 'None',
+            'samesite' => $isSecure ? 'None' : 'Lax',
         ];
 
         $cookieOptions = array_merge($defaultOptions, $options);
