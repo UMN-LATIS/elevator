6694a4dbef35fe0fee2d18c9289cb3dbed769550
James Johnson <johnsojr@umn.edu>
2026-06-18 20:05:38 -0500

    add GLOBAL_GROUPS to controller

--- application/controllers/AdminPermissions.php
+++ application/controllers/AdminPermissions.php
@@ -7,12 +7,28 @@ use App\Enums\GroupType;
  * pure json api for new ui
  */
 class AdminPermissions extends Instance_Controller {
+  // mirrors the structure of AuthHelpers::$authTypes,
+  // @see UMNHelper:$authTypes for an example
+  const GLOBAL_GROUPS = [
+    ALL_TYPE => [
+      "name" => ALL_TYPE,
+      "label" => "All",
+      "helpText" => "Matches everyone, including signed-out visitors."
+    ],
+     AUTHED_TYPE => [
+      "name" => AUTHED_TYPE,
+      "label" => "Authenticated Users"
+    ],
+     REMOTE_TYPE => [
+      "name" => REMOTE_TYPE,
+      "label" => "Centrally Authenticated Users"
+    ],
+     USER_TYPE => [
+      "name" => USER_TYPE,
+      "label" => "Specific People"
+    ],
+   ];
 
-  public function __construct() {
-    parent::__construct();
-
-    $this->load->library('SimpleValidator');
-  }
 
   public function groupTypes() {
     $this->abortUnlessAdmin();
