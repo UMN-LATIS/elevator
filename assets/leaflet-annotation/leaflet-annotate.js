10af521c0330661ec0e9b577f5f9d899a3d0b857
Colin McFadden <cmcfadden@gmail.com>
2026-07-17 15:54:41 -0500

    Revert "more logs"
    This reverts commit d39f7f9e4ed479a7a26e0cfc7d5dad37ffa89f80.

--- assets/leaflet-annotation/leaflet-annotate.js
+++ assets/leaflet-annotation/leaflet-annotate.js
@@ -31,7 +31,7 @@ function LAnnotate (viewer, options,sidecar) {
 
 
   this.arrowOptions = {
-    distanceUnit: 'px', //CRS.Simple uses pixel coordinates, not kilometers
+    distanceUnit: 'km', //yeah yeah, by default, the distance is in KILOMETERS because this is mapping software...
     stretchFactor: 1.11,
     popupContent: function(data) {
       return '<em>' + data.title + '</em>'
