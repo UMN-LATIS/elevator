d2ea90297d1a9995be3baffc6ebefc2fb4ec6294
James Johnson <johnsojr@umn.edu>
2026-08-18 10:55:12 -0500

    include showAssetLastModifiedDate in defaults

--- application/models/Entity/Instance.php
+++ application/models/Entity/Instance.php
@@ -1318,6 +1318,7 @@ class Instance
         $defaults = [
             'showChildCollections' => true,
             'showThumbnailDescription' => false,
+            'showAssetLastModifiedDate' => false,
         ];
 
         return array_merge($defaults, $this->additionalSettings ?? []);
@@ -1344,11 +1345,17 @@ class Instance
         return $this;
     }
 
+    /**
+     * Whether an asset's last-modified date appears alongside it.
+     */
     public function getShowAssetLastModifiedDate(): bool
     {
-        return (bool) $this->getAdditionalSettings()['showAssetLastModifiedDate'] ?? false;
+        return (bool) $this->getAdditionalSettings()['showAssetLastModifiedDate'];
     }
 
+    /**
+     * Write showAssetLastModifiedDate into the stored blob, preserving sibling keys.
+     */
     public function setShowAssetLastModifiedDate(bool $value): self
     {
         $this->additionalSettings = array_merge(
