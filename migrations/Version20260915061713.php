db7785944fb8a47d8807482ec2752fc16f9bcb2d
Colin McFadden <cmcfadden@gmail.com>
2026-09-15 08:19:36 +0200

    add migration

---
+++ migrations/Version20260915061713.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Elevator\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260915061713 extends AbstractMigration {
+    public function getDescription(): string {
+        return 'Add edittemplates permission';
+    }
+
+    public function up(Schema $schema): void {
+
+        $schema->getConnection()->executeStatement('
+            INSERT INTO "public"."permissions"("name","label","level","createdat","modifiedat","id")
+            VALUES
+            (\'edittemplates\',\'Edit Templates\',\'57\',NULL,NULL,11);
+        ');
+        // this up() migration is auto-generated, please modify it to your needs
+
+    }
+
+    public function down(Schema $schema): void {
+        $schema->getConnection()->executeStatement('
+            DELETE FROM "public"."permissions" WHERE "id" = 11;
+        ');
+    }
+}
